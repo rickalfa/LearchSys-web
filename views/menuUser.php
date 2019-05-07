f7cf743e2854e4660db71c46dfccf53bfb9d8eab
ricardo betancourt <angelshamael@gmail.com>
2019-05-06 23:58:37 -0400

    menu compeltado para usuarion no logeados

---
+++ views/menuUser.php
@@ -0,0 +1,29 @@
+<!-- NAVEGATION MENU -->
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+
+
+<a class="navbar-brand" href="#">System Roble</a>
+
+  <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+
+    <li class=nav-item active>
+      <a class="nav-link" href="home">Home <span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="notice">Notice</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="vision">vision</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="download">Download</a>
+    </li>"
+    <li class="nav-item">
+      <a class="nav-link" href="member">Members</a>
+    </li>"
+
+
+  </ul>
+</nav> 
+<!-- END  NAVEGATION MENU -->
